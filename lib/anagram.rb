d61963aaa001992ee03805364a9fdc97ca2a367d
Jared Grove <jaredkgrove@gmail.com>
2019-03-28 02:31:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,7 @@ class Anagram
   
   def match(anagrams)
     anagrams.select do |anagram_word|
-      anagram_word.split("").all? do |letter|
-        word.split("").include?(letter)
-      end
+      anagram_word.split("").all? {|letter| word.split("").include?(letter)} & anagram_word.length = word.length
     end
   end
 end
